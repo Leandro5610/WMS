9adde70914f2ffb7070649e43a46010c3518aae4
passosrenan <passosrenan325@gmail.com>
2022-10-13 23:22:57 -0300

    relatorio

--- src/main/java/senai/sp/cotia/wms/services/EmailService.java
+++ src/main/java/senai/sp/cotia/wms/services/EmailService.java
@@ -13,40 +13,40 @@ import senai.sp.cotia.wms.repository.EmailRepository;
 import senai.sp.cotia.wms.type.StatusEmail;
 
 
-@Service
-public class EmailService {
- 
-
-    @Autowired
-    EmailRepository emailRepository;
-
- 
-
-    @Autowired
-    private JavaMailSender emailSender;
-
- 
-
-    public EmailModel sendEmail(EmailModel emailModel) {
-        emailModel.setSendDateEmail(LocalDateTime.now());
-        try{
-        	//criando e instanciando um email com os atributos
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(emailModel.getEmailFrom());
-            message.setTo(emailModel.getEmailTo());
-            message.setSubject(emailModel.getSubject());
-            message.setText(emailModel.getText());
-            emailSender.send(message);
-
- 
-            //setando o status do email como enviado, se caso algo impedisse, avisa o erro
-            emailModel.setStatusEmail(StatusEmail.SENT);
-        } catch (MailException e){
-        	System.out.println(e.getMessage());
-            e.printStackTrace();            
-            emailModel.setStatusEmail(StatusEmail.ERROR);
-        }
-        return emailRepository.save(emailModel);
-    }
-    
-}    
+//@Service
+//public class EmailService {
+// 
+//
+//    @Autowired
+//    EmailRepository emailRepository;
+//
+// 
+//
+//    @Autowired
+//    private JavaMailSender emailSender;
+//
+// 
+//
+//    public EmailModel sendEmail(EmailModel emailModel) {
+//        emailModel.setSendDateEmail(LocalDateTime.now());
+//        try{
+//        	//criando e instanciando um email com os atributos
+//            SimpleMailMessage message = new SimpleMailMessage();
+//            message.setFrom(emailModel.getEmailFrom());
+//            message.setTo(emailModel.getEmailTo());
+//            message.setSubject(emailModel.getSubject());
+//            message.setText(emailModel.getText());
+//            emailSender.send(message);
+//
+// 
+//            //setando o status do email como enviado, se caso algo impedisse, avisa o erro
+//            emailModel.setStatusEmail(StatusEmail.SENT);
+//        } catch (MailException e){
+//        	System.out.println(e.getMessage());
+//            e.printStackTrace();            
+//            emailModel.setStatusEmail(StatusEmail.ERROR);
+//        }
+//        return emailRepository.save(emailModel);
+//    }
+//    
+//}    
