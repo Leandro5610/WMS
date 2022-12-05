dd3449bf2c20bff96cf2765338be06ac016baad6
Leandro5610 <leandrorviana56@gmail.com>
2022-12-05 20:58:49 -0300

    metodo para adicionar professor temporario para login

--- src/main/java/senai/sp/cotia/wms/WmsApplication.java
+++ src/main/java/senai/sp/cotia/wms/WmsApplication.java
@@ -1,19 +1,30 @@
 package senai.sp.cotia.wms;
 
 import java.io.File;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import senai.sp.cotia.wms.model.Professor;
+import senai.sp.cotia.wms.repository.ProdutoRepository;
+import senai.sp.cotia.wms.repository.ProfessorRepository;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableScheduling
 public class WmsApplication {
+	@Autowired
+	private ProfessorRepository repository;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
 		SpringApplication.run(WmsApplication.class, args);
+		
 	}
 
 
@@ -26,13 +37,26 @@ public class WmsApplication {
 		String nameXml = new File("src\\main\\resources\\static\\relatorios").getAbsolutePath();
 		File file = new File(nameXml);
 
-
 		for (File f : file.listFiles()) {
 			f.delete();
 		}
 
 	}
-
-
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void saveProf() {
+		List<Professor> prof = repository.findAll();
+		Professor profPadrao = new Professor();
+		
+		if (prof.isEmpty()) {
+			profPadrao.setEmail("admin@gmail.com");
+			profPadrao.setNif("1234");
+			profPadrao.setNome("Admin");
+			profPadrao.setSenha("admin");
+			repository.save(profPadrao);
+		}else {
+			System.out.println("Não é necessário fazer cadastro");
+		}
+	}
 }
 	
